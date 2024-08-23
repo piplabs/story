c82ffa5b5641cadf10e72de2b087f665322ccb4f
Jongwon Park <jwparktom@gmail.com>
2024-08-24 03:35:49 +0900

    feat(contracts): deploy all core contracts and test (#32)

--- contracts/test/script/DeployCore.t.sol
+++ contracts/test/script/DeployCore.t.sol
@@ -7,28 +7,24 @@ pragma solidity ^0.8.23;
 
 import { Test } from "forge-std/Test.sol";
 
-import { DeployIPTokenStaking } from "../../script/DeployIPTokenStaking.sol";
+import { DeployCore } from "../../script/DeployCore.s.sol";
 
-contract DeployIPTokenStakingTest is Test {
-    DeployIPTokenStaking private deployIPTokenStaking;
+contract DeployCoreTest is Test {
+    DeployCore private deployCore;
 
     function setUp() public {
-        deployIPTokenStaking = new DeployIPTokenStaking();
+        deployCore = new DeployCore();
     }
 
-    function testDeployIPTokenStaking_run() public {
-        // Network shall not deploy the IPTokenStaking contract if not mainnet.
-        vm.expectRevert("only mainnet deployment");
-        deployIPTokenStaking.run();
-
+    function testDeployDeployCore_run() public {
         // Network shall not deploy the IPTokenStaking contract if IPTOKENSTAKING_DEPLOYER_KEY not set.
-        vm.chainId(1);
+        vm.chainId(1513);
         // solhint-disable
-        vm.expectRevert("vm.envUint: environment variable \"IPTOKENSTAKING_DEPLOYER_KEY\" not found");
-        deployIPTokenStaking.run();
+        vm.expectRevert('vm.envUint: environment variable "IPTOKENSTAKING_DEPLOYER_KEY" not found');
+        deployCore.run();
 
         // Network shall deploy the IPTokenStaking contract.
         vm.setEnv("IPTOKENSTAKING_DEPLOYER_KEY", "0x123456789abcdef");
-        deployIPTokenStaking.run();
+        deployCore.run();
     }
 }
