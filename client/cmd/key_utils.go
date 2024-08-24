3db3170523fa7ca8f84894dae9f41668f213c491
Leeren Chang <leeren@leerenchang.com>
2024-08-24 04:13:57 -0700

    feat(client): update validator cli

---
+++ client/cmd/key_utils.go
@@ -0,0 +1,104 @@
+package cmd
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/piplabs/story/lib/errors"
+)
+
+type ValidatorKey struct {
+	Address string  `json:"address"`
+	PubKey  KeyInfo `json:"pub_key"`
+	PrivKey KeyInfo `json:"priv_key"`
+}
+
+type KeyInfo struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+func decodeAndUncompressPubKey(compressedPubKeyBase64 string) (string, error) {
+	compressedPubKeyBytes, err := base64.StdEncoding.DecodeString(compressedPubKeyBase64)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to decode base64 public key")
+	}
+	if len(compressedPubKeyBytes) != 33 {
+		return "", fmt.Errorf("invalid compressed public key length: %d", len(compressedPubKeyBytes))
+	}
+
+	curve := elliptic.P256()
+	x, y := elliptic.UnmarshalCompressed(curve, compressedPubKeyBytes)
+	if x == nil || y == nil {
+		return "", errors.New("failed to unmarshal compressed public key")
+	}
+
+	uncompressedPubKeyBytes := elliptic.Marshal(curve, x, y)
+	uncompressedPubKeyHex := hex.EncodeToString(uncompressedPubKeyBytes)
+
+	return uncompressedPubKeyHex, nil
+}
+
+func deriveUncompressedPublicKeyFromPrivateKey(evmPrivKey *ecdsa.PrivateKey) ([]byte, error) {
+	pubKey := evmPrivKey.PublicKey
+	uncompressedPubKey := elliptic.Marshal(pubKey.Curve, pubKey.X, pubKey.Y)
+	if len(uncompressedPubKey) != 65 {
+		return nil, fmt.Errorf("invalid uncompressed public key length: %d", len(uncompressedPubKey))
+	}
+
+	return uncompressedPubKey, nil
+}
+
+func validatorKeyExport(keyFilePath string) error {
+	keyFileBytes, err := os.ReadFile(keyFilePath)
+	if err != nil {
+		return errors.Wrap(err, "failed to read key file")
+	}
+
+	var keyData ValidatorKey
+	if err := json.Unmarshal(keyFileBytes, &keyData); err != nil {
+		return errors.Wrap(err, "failed to unmarshal key file")
+	}
+
+	privKeyBytes, err := base64.StdEncoding.DecodeString(keyData.PrivKey.Value)
+	if err != nil {
+		return errors.Wrap(err, "failed to decode private key")
+	}
+
+	privateKey, err := crypto.ToECDSA(privKeyBytes)
+	if err != nil {
+		return errors.Wrap(err, "invalid private key")
+	}
+
+	publicKey, ok := privateKey.Public().(*ecdsa.PublicKey)
+	if !ok {
+		return errors.New("failed to cast public key to ecdsa.PublicKey")
+	}
+	evmPublicKey := crypto.PubkeyToAddress(*publicKey).Hex()
+
+	// Handle the compressed public key
+	compressedPubKeyBytes, err := base64.StdEncoding.DecodeString(keyData.PubKey.Value)
+	if err != nil {
+		return errors.Wrap(err, "failed to decode base64 public key")
+	}
+	compressedPubKeyHex := hex.EncodeToString(compressedPubKeyBytes)
+
+	// Get the uncompressed public key using the refactored function
+	uncompressedPubKeyHex, err := decodeAndUncompressPubKey(keyData.PubKey.Value)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("------------------------------------------------------")
+	fmt.Println("EVM Public Key:", evmPublicKey)
+	fmt.Println("Compressed Public Key:", compressedPubKeyHex)
+	fmt.Println("Uncompressed Public Key:", uncompressedPubKeyHex)
+	fmt.Println("------------------------------------------------------")
+	return nil
+}
---
+++ client/cmd/transaction.go
@@ -0,0 +1,114 @@
+package cmd
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/piplabs/story/lib/errors"
+)
+
+func prepareAndSendTransaction(ctx context.Context, cfg baseConfig, contractAddress common.Address, value *big.Int, data []byte) error {
+	client, err := ethclient.Dial(cfg.RPC)
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to Ethereum client")
+	}
+
+	evmPrivKey, err := crypto.HexToECDSA(cfg.PrivateKey)
+	if err != nil {
+		return errors.Wrap(err, "invalid EVM private key")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*evmPrivKey.Public().(*ecdsa.PublicKey))
+	nonce, err := client.PendingNonceAt(ctx, fromAddress)
+	if err != nil {
+		return errors.Wrap(err, "failed to get nonce")
+	}
+
+	gasPrice, err := client.SuggestGasPrice(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to suggest gas price")
+	}
+	gasPrice = new(big.Int).Mul(gasPrice, big.NewInt(120))
+	gasPrice = new(big.Int).Div(gasPrice, big.NewInt(100))
+
+	gasLimit, err := estimateGas(ctx, client, fromAddress, contractAddress, gasPrice, value, data)
+	if err != nil {
+		return err
+	}
+
+	gasTipCap := gasPrice
+	gasFeeCap := new(big.Int).Mul(gasPrice, big.NewInt(2))
+
+	gasCost := new(big.Int).Mul(big.NewInt(int64(gasLimit)), gasFeeCap)
+	totalTxCost := new(big.Int).Add(gasCost, value)
+
+	balance, err := client.BalanceAt(ctx, fromAddress, nil)
+	if err != nil {
+		return errors.Wrap(err, "failed to fetch balance")
+	}
+
+	if balance.Cmp(totalTxCost) < 0 {
+		return errors.New("insufficient funds for gas * price + value", "balance", balance.String(), "totalTxCost", totalTxCost.String())
+	}
+
+	tx := types.NewTx(&types.DynamicFeeTx{
+		ChainID:   big.NewInt(cfg.ChainID),
+		Nonce:     nonce,
+		GasFeeCap: gasFeeCap,
+		GasTipCap: gasTipCap,
+		Gas:       gasLimit,
+		To:        &contractAddress,
+		Value:     value,
+		Data:      data,
+	})
+
+	signedTx, err := types.SignTx(tx, types.LatestSignerForChainID(big.NewInt(cfg.ChainID)), evmPrivKey)
+	if err != nil {
+		return errors.Wrap(err, "failed to sign transaction")
+	}
+
+	txHash := signedTx.Hash().Hex()
+	fmt.Printf("Transaction hash: %s\n", txHash)
+	fmt.Printf("Explorer URL: %s/tx/%s\n", cfg.Explorer, txHash)
+
+	if err = client.SendTransaction(ctx, signedTx); err != nil {
+		return errors.Wrap(err, "failed to send transaction")
+	}
+
+	fmt.Println("Transaction sent, waiting for confirmation...")
+
+	receipt, err := bind.WaitMined(ctx, client, signedTx)
+	if err != nil {
+		return errors.Wrap(err, "transaction failed")
+	}
+
+	if receipt.Status == types.ReceiptStatusFailed {
+		return errors.New("transaction failed", "status", receipt.Status)
+	}
+
+	fmt.Println("Transaction confirmed successfully!")
+	return nil
+}
+
+func estimateGas(ctx context.Context, client *ethclient.Client, fromAddress, contractAddress common.Address, gasPrice, value *big.Int, data []byte) (uint64, error) {
+	msg := ethereum.CallMsg{
+		From:     fromAddress,
+		To:       &contractAddress,
+		GasPrice: gasPrice,
+		Value:    value,
+		Data:     data,
+	}
+	gasLimit, err := client.EstimateGas(ctx, msg)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to estimate gas")
+	}
+	return gasLimit, nil
+}
