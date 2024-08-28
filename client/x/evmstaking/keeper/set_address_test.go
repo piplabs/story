23227a9d8644f88b4d7477a594bb1f47fec70034
zsystm <actor93kor@gmail.com>
2024-08-28 12:48:11 +0900

    test(evmstaking/keeper): add test cases for set_address
    coverage:
    - Increased test coverage from 0% to 87.5%

---
+++ client/x/evmstaking/keeper/set_address_test.go
@@ -0,0 +1,94 @@
+package keeper_test
+
+import (
+	"github.com/cometbft/cometbft/crypto"
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/piplabs/story/contracts/bindings"
+	"github.com/piplabs/story/lib/k1util"
+)
+
+func (s *TestSuite) TestProcessSetWithdrawalAddress() {
+	require := s.Require()
+	ctx, keeper := s.Ctx, s.EVMStakingKeeper
+
+	pubKeys, accAddrs, _ := createAddresses(2)
+	delAddr := accAddrs[0]
+	delPubKey := pubKeys[0]
+	corruptedPubKey := delPubKey.Bytes()[0:20]
+
+	execAddr, err := k1util.CosmosPubkeyToEVMAddress(delPubKey.Bytes())
+	require.NoError(err)
+	anotherExecAddr, err := k1util.CosmosPubkeyToEVMAddress(pubKeys[1].Bytes())
+	require.NoError(err)
+
+	tcs := []struct {
+		name          string
+		sameAddr      bool
+		input         func(execAddr common.Address, pubKey crypto.PubKey) *bindings.IPTokenStakingSetWithdrawalAddress
+		expectedError string
+	}{
+		{
+			name:     "pass: same delegator and execution address",
+			sameAddr: true,
+			input: func(execAddr common.Address, pubKey crypto.PubKey) *bindings.IPTokenStakingSetWithdrawalAddress {
+				paddedExecAddr := common.LeftPadBytes(execAddr.Bytes(), 32)
+				return &bindings.IPTokenStakingSetWithdrawalAddress{
+					DelegatorCmpPubkey: pubKey.Bytes(),
+					ExecutionAddress:   [32]byte(paddedExecAddr),
+				}
+			},
+			expectedError: "",
+		},
+		{
+			name:     "pass: same delegator and different execution address",
+			sameAddr: false,
+			input: func(execAddr common.Address, pubKey crypto.PubKey) *bindings.IPTokenStakingSetWithdrawalAddress {
+				paddedExecAddr := common.LeftPadBytes(execAddr.Bytes(), 32)
+				return &bindings.IPTokenStakingSetWithdrawalAddress{
+					DelegatorCmpPubkey: pubKey.Bytes(),
+					ExecutionAddress:   [32]byte(paddedExecAddr),
+				}
+			},
+			expectedError: "",
+		},
+		{
+			name:     "fail: delegator key is corrupted",
+			sameAddr: false,
+			input: func(execAddr common.Address, pubKey crypto.PubKey) *bindings.IPTokenStakingSetWithdrawalAddress {
+				paddedExecAddr := common.LeftPadBytes(execAddr.Bytes(), 32)
+				return &bindings.IPTokenStakingSetWithdrawalAddress{
+					DelegatorCmpPubkey: corruptedPubKey,
+					ExecutionAddress:   [32]byte(paddedExecAddr),
+				}
+			},
+			expectedError: "depositor pubkey to cosmos",
+		},
+	}
+
+	for _, tc := range tcs {
+		tc := tc
+		s.Run(tc.name, func() {
+			var ev *bindings.IPTokenStakingSetWithdrawalAddress
+			evmAddr := execAddr
+			if !tc.sameAddr {
+			} else {
+				evmAddr = anotherExecAddr
+			}
+			ev = tc.input(evmAddr, pubKeys[0])
+
+			// Process the event
+			err := keeper.ProcessSetWithdrawalAddress(ctx, ev)
+			if tc.expectedError != "" {
+				require.Error(err)
+				require.Contains(err.Error(), tc.expectedError)
+			} else {
+				require.NoError(err)
+				// check result
+				addr, err := keeper.DelegatorMap.Get(ctx, delAddr.String())
+				require.NoError(err)
+				require.Equal(evmAddr.String(), addr)
+			}
+		})
+	}
+}
