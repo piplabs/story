32967c797dd673da384bd99038c0b07b467c4f57
zsystm <actor93kor@gmail.com>
2024-09-05 15:04:47 +0900

    refactor test codes and add tc for mixed case
    mixed case means there are matured unbonding and not matured unbonding.

--- client/x/evmstaking/keeper/staking_queue_test.go
+++ client/x/evmstaking/keeper/staking_queue_test.go
@@ -6,48 +6,11 @@ import (
 
 	"cosmossdk.io/math"
 
-	"github.com/cometbft/cometbft/crypto"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	skeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
-	"github.com/cosmos/cosmos-sdk/x/staking/testutil"
 	stypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-
-	"github.com/piplabs/story/lib/k1util"
 
 	"go.uber.org/mock/gomock"
 )
-
-// setupValidatorAndDelegation creates a validator and delegation for testing.
-func (s *TestSuite) setupValidatorAndDelegation(ctx sdk.Context, valPubKey, delPubKey crypto.PubKey, valAddr sdk.ValAddress, delAddr sdk.AccAddress) {
-	require := s.Require()
-	bankKeeper, stakingKeeper, keeper := s.BankKeeper, s.StakingKeeper, s.EVMStakingKeeper
-
-	// Convert public key to cosmos format
-	valCosmosPubKey, err := k1util.PubKeyToCosmos(valPubKey)
-	require.NoError(err)
-
-	// Create and update validator
-	val := testutil.NewValidator(s.T(), valAddr, valCosmosPubKey)
-	valTokens := stakingKeeper.TokensFromConsensusPower(ctx, 10)
-	validator, _ := val.AddTokensFromDel(valTokens)
-	bankKeeper.EXPECT().SendCoinsFromModuleToModule(gomock.Any(), stypes.NotBondedPoolName, stypes.BondedPoolName, gomock.Any())
-	_ = skeeper.TestingUpdateValidator(stakingKeeper, ctx, validator, true)
-
-	// Create and set delegation
-	delAmt := stakingKeeper.TokensFromConsensusPower(ctx, 100).ToLegacyDec()
-	delegation := stypes.NewDelegation(delAddr.String(), valAddr.String(), delAmt)
-	require.NoError(stakingKeeper.SetDelegation(ctx, delegation))
-
-	// Map delegator to EVM address
-	delEvmAddr, err := k1util.CosmosPubkeyToEVMAddress(delPubKey.Bytes())
-	require.NoError(err)
-	require.NoError(keeper.DelegatorMap.Set(ctx, delAddr.String(), delEvmAddr.String()))
-
-	// Ensure delegation is set correctly
-	delegation, err = stakingKeeper.GetDelegation(ctx, delAddr, valAddr)
-	require.NoError(err)
-	require.Equal(delAmt, delegation.GetShares())
-}
 
 // setupUnbonding creates unbondings for testing.
 func (s *TestSuite) setupUnbonding(ctx context.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress, amount string) {
@@ -57,6 +20,16 @@ func (s *TestSuite) setupUnbonding(ctx context.Context, delAddr sdk.AccAddress, 
 	bankKeeper.EXPECT().SendCoinsFromModuleToModule(gomock.Any(), stypes.BondedPoolName, stypes.NotBondedPoolName, gomock.Any())
 	_, _, err := stakingKeeper.Undelegate(ctx, delAddr, valAddr, math.LegacyMustNewDecFromStr(amount))
 	require.NoError(err)
+}
+
+// setupMaturedUnbonding creates matured unbondings for testing.
+func (s *TestSuite) setupMatureUnbonding(ctx context.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress, amt string, duration time.Duration) {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	pastHeader := sdkCtx.BlockHeader()
+	pastHeader.Time = pastHeader.Time.Add(-duration).Add(-time.Minute)
+	pastCtx := sdkCtx.WithBlockHeader(pastHeader)
+
+	s.setupUnbonding(pastCtx, delAddr, valAddr, amt)
 }
 
 func (s *TestSuite) TestGetMatureUnbondedDelegations() {
@@ -74,12 +47,20 @@ func (s *TestSuite) TestGetMatureUnbondedDelegations() {
 	pubKeys, accAddrs, valAddrs := createAddresses(3)
 	delAddr := accAddrs[0]
 	delPubKey := pubKeys[0]
-	valPubKey := pubKeys[1]
-	valAddr := valAddrs[1]
+	valPubKey1 := pubKeys[1]
+	valAddr1 := valAddrs[1]
 	valPubKey2 := pubKeys[2]
 	valAddr2 := valAddrs[2]
-	s.setupValidatorAndDelegation(ctx, valPubKey, delPubKey, valAddr, delAddr)
+	s.setupValidatorAndDelegation(ctx, valPubKey1, delPubKey, valAddr1, delAddr)
 	s.setupValidatorAndDelegation(ctx, valPubKey2, delPubKey, valAddr2, delAddr)
+
+	// set staking module's params
+	ubdTime := time.Duration(3600) * time.Second
+	stakingParams, err := stakingKeeper.GetParams(ctx)
+	require.NoError(err)
+	stakingParams.UnbondingTime = ubdTime
+	require.NoError(err)
+	require.NoError(stakingKeeper.SetParams(ctx, stakingParams))
 
 	tcs := []struct {
 		name           string
@@ -88,27 +69,31 @@ func (s *TestSuite) TestGetMatureUnbondedDelegations() {
 		expectedError  string
 	}{
 		{
-			name:           "pass: no unbondings",
+			name: "pass: no matured unbondings",
+			setUnbondings: func(c context.Context) {
+				s.setupUnbonding(c, delAddr, valAddr1, "100")
+				s.setupUnbonding(c, delAddr, valAddr2, "100")
+			},
 			expectedResult: nil,
 		},
 		{
-			name: "pass: one unbonding",
+			name: "pass: one mautred and one not matured unbonding",
 			setUnbondings: func(c context.Context) {
-				s.setupUnbonding(c, delAddr, valAddr, "100")
+				s.setupMatureUnbonding(c, delAddr, valAddr1, "100", ubdTime)
+				s.setupUnbonding(c, delAddr, valAddr2, "100")
 			},
 			expectedResult: []stypes.DVPair{
-				{DelegatorAddress: delAddr.String(), ValidatorAddress: valAddr.String()},
+				{DelegatorAddress: delAddr.String(), ValidatorAddress: valAddr1.String()},
 			},
 		},
 		{
-			name: "pass: multiple unbondings",
+			name: "pass: two matured unbondings",
 			setUnbondings: func(c context.Context) {
-				// Set up multiple unbondings
-				s.setupUnbonding(c, delAddr, valAddr, "50")
-				s.setupUnbonding(c, delAddr, valAddr2, "50")
+				s.setupMatureUnbonding(c, delAddr, valAddr1, "100", ubdTime)
+				s.setupMatureUnbonding(c, delAddr, valAddr2, "100", ubdTime)
 			},
 			expectedResult: []stypes.DVPair{
-				{DelegatorAddress: delAddr.String(), ValidatorAddress: valAddr.String()},
+				{DelegatorAddress: delAddr.String(), ValidatorAddress: valAddr1.String()},
 				{DelegatorAddress: delAddr.String(), ValidatorAddress: valAddr2.String()},
 			},
 		},
@@ -121,10 +106,7 @@ func (s *TestSuite) TestGetMatureUnbondedDelegations() {
 				tc.setUnbondings(cachedCtx)
 			}
 
-			// Set block time to be after unbonding period
-			header := cachedCtx.BlockHeader()
-			header.Time = header.Time.Add(params.UnbondingTime).Add(24 * time.Hour)
-			result, err := keeper.GetMatureUnbondedDelegations(cachedCtx.WithBlockHeader(header))
+			result, err := keeper.GetMatureUnbondedDelegations(cachedCtx)
 
 			// Run the test
 			if tc.expectedError != "" {
