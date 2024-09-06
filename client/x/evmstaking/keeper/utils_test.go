bb967a44cbca0242bc0d820d858f58820084f491
zsystm <124245155+zsystm@users.noreply.github.com>
2024-09-06 14:08:54 +0900

    test(evmstaking/keeper): add test cases for utils (#76)

---
+++ client/x/evmstaking/keeper/utils_test.go
@@ -0,0 +1,23 @@
+package keeper_test
+
+import (
+	"math/big"
+	"testing"
+
+	"cosmossdk.io/math"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/piplabs/story/client/x/evmstaking/keeper"
+)
+
+func TestIPTokenToBondCoin(t *testing.T) {
+	t.Parallel()
+	amount := big.NewInt(100)
+	coin, coins := keeper.IPTokenToBondCoin(amount)
+	require.Equal(t, sdk.DefaultBondDenom, coin.Denom)
+	require.Equal(t, math.NewIntFromBigInt(amount), coin.Amount)
+	require.Equal(t, sdk.NewCoin(sdk.DefaultBondDenom, math.NewIntFromBigInt(amount)), coin)
+	require.Equal(t, sdk.NewCoins(coin), coins)
+}
