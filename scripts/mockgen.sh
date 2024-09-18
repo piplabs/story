2c55d0d85b389d537fd0eba534c566aa2f15df73
Narangde <hansol.lee@storyprotocol.xyz>
2024-09-18 09:29:16 +0900

    chore: modify path for mockgen (#121)

--- scripts/mockgen.sh
+++ scripts/mockgen.sh
@@ -3,5 +3,5 @@
 mockgen_cmd="mockgen"
 $mockgen_cmd -package mock -destination testutil/mock/grpc_server.go github.com/cosmos/gogoproto/grpc Server
 $mockgen_cmd -package mock -destination testutil/mock/logger.go cosmossdk.io/log Logger
-$mockgen_cmd -source=client/x/evmengine/types/expected_keepers.go -package testutil -destination client/x/evmengine/testutil/expected_keepers_mocks.go
-$mockgen_cmd -source=client/x/evmstaking/types/expected_keepers.go -package testutil -destination client/x/evmstaking/testutil/expected_keepers_mocks.go
+$mockgen_cmd -source=../client/x/evmengine/types/expected_keepers.go -package testutil -destination ../client/x/evmengine/testutil/expected_keepers_mocks.go
+$mockgen_cmd -source=../client/x/evmstaking/types/expected_keepers.go -package testutil -destination ../client/x/evmstaking/testutil/expected_keepers_mocks.go
