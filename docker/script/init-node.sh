1299d364341065e5f7561a1efb55bfe2f2a99a95
haodi <82733821@qq.com>
2024-09-22 21:09:08 +0800

    adapt cosmovisor

--- docker/script/init-node.sh
+++ docker/script/init-node.sh
@@ -1,34 +1,49 @@
 #!/usr/bin/env bash
 
 if [ -z "$NETWORK" ]; then
-    NETWORK="public-testnet"
+    NETWORK="iliad"
 fi
 
-DATA_DIR="/data"
+DATA_DIR="$HOME/.story"
+COSMOVISOR_FILE_NAME="public-testnet"
 INIT_DONE_FILE="$DATA_DIR/.init_done"
-CONFIG_SOURCE="/init-config/$NETWORK"
 
-if [ ! -d "$CONFIG_SOURCE" ]; then
-    echo "network '$NETWORK' config not exists."
-    exit 1
-fi
+export DAEMON_NAME="story"
+export DAEMON_HOME="/home/ec2-user/story"
+
+case $NETWORK in
+    iliad)
+        echo "Use iliad as network ..."
+        COSMOVISOR_FILE_NAME="public-testnet"
+        ;;
+    *)
+        echo "Invalid Network. Use iliad."
+        exit 1
+        ;;
+esac
 
 if [ -f "$INIT_DONE_FILE" ]; then
     echo "skip init, run node..."
 else
-    echo "init config..."
-    cp -r "$CONFIG_SOURCE/"* "/data/"
+    echo "init network..."
+    mkdir -p /home/ec2-user/story
+    mkdir $DAEMON_HOME/data $DAEMON_HOME/backup
 
-    story-init --config-dir="$DATA_DIR"
-    geth --state.scheme "hash" --gcmode archive init --datadir="$DATA_DIR/geth/data" $DATA_DIR/geth/config/genesis.json
-    openssl rand -hex 32 > $DATA_DIR/geth/data/geth/jwtsecret
+    # init cosmovisor data
+    wget -q https://story-geth-binaries.s3.us-west-1.amazonaws.com/cosmovisor/$COSMOVISOR_FILE_NAME.tar.gz -O cosmovisor_data.tar.gz
+    tar -zxf cosmovisor_data.tar.gz && rm -f cosmovisor_data.tar.gz
+    mv cosmovisor $DAEMON_HOME/
+
+    # init story data
+    $DAEMON_HOME/cosmovisor/genesis/bin/story init --network iliad
 
     touch "$INIT_DONE_FILE"
 fi
 
 echo "node run..."
-geth --config $DATA_DIR/geth/config/geth.toml --nodekey $DATA_DIR/geth/config/nodekey --metrics --metrics.addr 0.0.0.0 &
-story run --home=$DATA_DIR/story &
+geth --iliad --syncmode full --http --http.addr 0.0.0.0 --http.vhosts '*' --ws --ws.addr 0.0.0.0 --ws.origins '*' --rpc.txfeecap 0 &
+sleep 5s
+cosmovisor run run --home=$DATA_DIR/story &
 
 wait -n
 exit $?
