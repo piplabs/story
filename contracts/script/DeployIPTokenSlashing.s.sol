85bb2b3c3e1a25fa98aa61ac65df824e77747bd8
Raul <raul.mf86@gmail.com>
2024-10-01 01:34:55 -0300

    use solady CREATE3 to deploy IPTokenSlashing deterministically

--- contracts/script/DeployIPTokenSlashing.s.sol
+++ contracts/script/DeployIPTokenSlashing.s.sol
@@ -7,6 +7,7 @@ import { Script } from "forge-std/Script.sol";
 import { console2 } from "forge-std/console2.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
+import { CREATE3 } from "solady/src/utils/CREATE3.sol";
 import { IPTokenSlashing } from "../src/protocol/IPTokenSlashing.sol";
 
 /**
@@ -20,7 +21,7 @@ contract DeployIPTokenSlashing is Script {
     //
     // - Deploy (OK for devnet)
     // forge script script/DeployIPTokenSlashing.s.sol --fork-url <fork-url> --broadcast
-    // 
+    //
     // - Deploy and Verify (for testnet)
     // forge script script/DeployIPTokenSlashing.s.sol --fork-url https://testnet.storyrpc.io --broadcast --verify --verifier blockscout --verifier-url https://testnet.storyscan.xyz/api\?
     function run() public {
@@ -29,30 +30,38 @@ contract DeployIPTokenSlashing is Script {
         require(protocolAccessManagerAddr != address(0), "address not set");
         // Read env for deployer private key
         uint256 deployerKey = vm.envUint("IPTOKENSTAKING_DEPLOYER_KEY");
-
+        address deployer = vm.addr(deployerKey);
+        console2.log("deployer", deployer);
         vm.startBroadcast(deployerKey);
 
         address ipTokenStaking = 0xCCcCcC0000000000000000000000000000000001;
 
         address impl = address(new IPTokenSlashing(ipTokenStaking));
-        IPTokenSlashing ipTokenSlashing = IPTokenSlashing(
-            address(
-                new ERC1967Proxy(
-                    impl,
-                    abi.encodeCall(
-                        IPTokenSlashing.initialize, (
-                            protocolAccessManagerAddr,
-                            1 ether // unjailFee
-                        )
-                    )
-                )
+        bytes memory initializationData = abi.encodeCall(
+            IPTokenSlashing.initialize,
+            (
+                protocolAccessManagerAddr,
+                1 ether // unjailFee
             )
         );
+        bytes memory creationCode =
+            abi.encodePacked(type(ERC1967Proxy).creationCode, abi.encode(impl, initializationData));
+        bytes32 salt = keccak256(abi.encode("STORY", type(IPTokenSlashing).name));
+        address predicted = CREATE3.predictDeterministicAddress(salt);
+        console2.log("IPTokenSlashing will be deployed at:", predicted);
 
-        vm.stopBroadcast();
+        IPTokenSlashing ipTokenSlashing = IPTokenSlashing(this.deployDeterministic(creationCode, salt));
 
         console2.log("IP_TOKEN_STAKING", address(ipTokenSlashing.IP_TOKEN_STAKING()));
+        if (address(ipTokenSlashing) != predicted) {
+            revert("IPTokenSlashing mismatch");
+        }
         console2.log("IPTokenSlashing deployed at:", address(ipTokenSlashing));
 
+        vm.stopBroadcast();
+    }
+
+    function deployDeterministic(bytes calldata creationCode, bytes32 salt) public returns (address) {
+        return CREATE3.deployDeterministic(0, creationCode, salt);
     }
 }
--- contracts/test/utils/Test.sol
+++ contracts/test/utils/Test.sol
@@ -10,6 +10,7 @@ import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy
 import { IPTokenStaking } from "../../src/protocol/IPTokenStaking.sol";
 import { IPTokenSlashing } from "../../src/protocol/IPTokenSlashing.sol";
 import { UpgradeEntrypoint } from "../../src/protocol/UpgradeEntrypoint.sol";
+import { CREATE3 } from "solady/src/utils/CREATE3.sol";
 
 contract Test is ForgeTest {
     address internal admin = address(0x123);
@@ -33,10 +34,7 @@ contract Test is ForgeTest {
                 500 // defaultMaxCommissionChangeRate, 5%
             )
         );
-        bytes memory initializer = abi.encodeCall(
-            IPTokenStaking.initialize,
-            (admin, 1 ether, 1 ether, 1 ether, 7 days)
-        );
+        bytes memory initializer = abi.encodeCall(IPTokenStaking.initialize, (admin, 1 ether, 1 ether, 1 ether, 7 days));
         ipTokenStaking = IPTokenStaking(address(new ERC1967Proxy(impl, initializer)));
     }
 
@@ -44,11 +42,25 @@ contract Test is ForgeTest {
         require(address(ipTokenStaking) != address(0), "ipTokenStaking not set");
 
         address impl = address(new IPTokenSlashing(address(ipTokenStaking)));
+        bytes memory initializationData = abi.encodeCall(
+            IPTokenSlashing.initialize,
+            (
+                admin,
+                1 ether // unjailFee
+            )
+        );
+        bytes memory creationCode =
+            abi.encodePacked(type(ERC1967Proxy).creationCode, abi.encode(impl, initializationData));
+        bytes32 salt = keccak256(abi.encode("STORY", type(IPTokenSlashing).name));
+        address predicted = CREATE3.predictDeterministicAddress(salt);
+        console2.log("IPTokenSlashing will be deployed at:", predicted);
 
-        bytes memory initializer = abi.encodeCall(IPTokenSlashing.initialize, (admin, 1 ether));
-        ipTokenSlashing = IPTokenSlashing(address(new ERC1967Proxy(impl, initializer)));
+        ipTokenSlashing = IPTokenSlashing(this.deployDeterministic(creationCode, salt));
 
-        console2.log("unjailFee:", ipTokenSlashing.unjailFee());
+        console2.log("IP_TOKEN_STAKING", address(ipTokenSlashing.IP_TOKEN_STAKING()));
+        if (address(ipTokenSlashing) != predicted) {
+            revert("IPTokenSlashing mismatch");
+        }
     }
 
     function setUpgrade() internal {
@@ -56,5 +68,9 @@ contract Test is ForgeTest {
 
         bytes memory initializer = abi.encodeWithSignature("initialize(address)", admin);
         upgradeEntrypoint = UpgradeEntrypoint(address(new ERC1967Proxy(impl, initializer)));
+    }
+
+    function deployDeterministic(bytes calldata creationCode, bytes32 salt) public returns (address) {
+        return CREATE3.deployDeterministic(0, creationCode, salt);
     }
 }
