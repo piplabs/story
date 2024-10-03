89cbde5f3ea4903665825e7454a004f3d2b94d2a
Raul <raul.mf86@gmail.com>
2024-10-03 20:30:20 -0300

    use create2 to deploy create3 deployer

--- contracts/script/DeployIPTokenSlashing.s.sol
+++ contracts/script/DeployIPTokenSlashing.s.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.23;
 import { Script } from "forge-std/Script.sol";
 import { console2 } from "forge-std/console2.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-
+import { Create2 } from "@openzeppelin/contracts/utils/Create2.sol";
 
 import { IPTokenSlashing } from "../src/protocol/IPTokenSlashing.sol";
 import { Create3 } from "../src/deploy/Create3.sol";
@@ -35,7 +35,10 @@ contract DeployIPTokenSlashing is Script {
         console2.log("deployer", deployer);
         vm.startBroadcast(deployerKey);
 
-        Create3 c3Deployer = new Create3();
+        bytes32 salt = keccak256(abi.encode("STORY", type(Create3).name));
+
+        Create3 c3Deployer = Create3(Create2.deploy(0, salt, type(Create3).creationCode));
+        console2.log("Create3 deployer:", address(c3Deployer));
 
         address ipTokenStaking = 0xCCcCcC0000000000000000000000000000000001;
 
@@ -49,13 +52,16 @@ contract DeployIPTokenSlashing is Script {
         );
         bytes memory creationCode =
             abi.encodePacked(type(ERC1967Proxy).creationCode, abi.encode(impl, initializationData));
-        bytes32 salt = keccak256(abi.encode("STORY", type(IPTokenSlashing).name));
+
+        salt = keccak256(abi.encode("STORY", type(IPTokenSlashing).name));
         address predicted = c3Deployer.getDeployed(salt);
         console2.log("IPTokenSlashing will be deployed at:", predicted);
         IPTokenSlashing ipTokenSlashing = IPTokenSlashing(c3Deployer.deploy(salt, creationCode));
 
         console2.log("IP_TOKEN_STAKING", address(ipTokenSlashing.IP_TOKEN_STAKING()));
         console2.log("owner:", ipTokenSlashing.owner());
+        console2.log("unjailFee:", ipTokenSlashing.unjailFee());
+
         if (address(ipTokenSlashing) != predicted) {
             revert("IPTokenSlashing mismatch");
         }
