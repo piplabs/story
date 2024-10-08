94ffc0959ae67ef5e176888acc2fbd388b6463f8
lutty <lutty@lutty.me>
2024-10-08 16:56:07 +0800

    add 1+2 1+4 support
    Signed-off-by: lutty <lutty@lutty.me>

--- docker/localnet/README.md
+++ docker/localnet/README.md
@@ -6,11 +6,10 @@
 2. Check the configuration in the `config` folder to see if any adjustments are necessary.
 3. Start the application using the command:
    ```bash
-   docker compose up -d
+   docker compose -f ./docker-compose-1+2.yaml up -d
    ```
 
 ## Important Notes
 
-1. The current Docker container will reset data upon each restart.
-2. In the configuration file, `10.22.22.1` serves as a placeholder IP for `bootnode1`, while `10.22.33.1` to `10.22.33.4` are placeholders for `validator1` to `validator4`. These will be dynamically replaced with container IPs during startup.
-3. If you are developing on macOS and seeking optimal performance, you can use the `linux/arm64` binary files. However, you must modify the `docker-compose.yaml` file to set the platform to `linux/arm64`.
+1. In the configuration file, `10.22.22.1` serves as a placeholder IP for `bootnode1`, while `10.22.33.1` is placeholders for `validator1` to `validator4`. These will be dynamically replaced with container IPs during startup.
+2. If you are developing on macOS and seeking optimal performance, you can use the `linux/arm64` binary files. However, you must modify the `docker-compose.yaml` file to set the platform to `linux/arm64`.
--- docker/localnet/script/init-node.sh
+++ docker/localnet/script/init-node.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 
-echo "node reset..."
-rm -rf /home/ec2-user
-cp -r /config /home/ec2-user
-geth --state.scheme "hash" --gcmode archive init --datadir="/home/ec2-user/geth/data" /home/ec2-user/geth/config/genesis.json
-openssl rand -hex 32 > /home/ec2-user/geth/data/geth/jwtsecret
-cat <<EOF > /home/ec2-user/story/data/priv_validator_state.json
-{
-  "height": "0",
-  "round": 0,
-  "step": 0
-}
-EOF
+DATA_DIR="/home/ec2-user"
+INIT_DONE_FILE="$DATA_DIR/.init_done"
 
-sed -i "s/10.22.33.1/$(nslookup validator1 | grep Address | head -n 2 | tail -n 1 | awk -F ': ' '{print $2}')/g" /home/ec2-user/story/config/config.toml
-sed -i "s/10.22.33.2/$(nslookup validator2 | grep Address | head -n 2 | tail -n 1 | awk -F ': ' '{print $2}')/g" /home/ec2-user/story/config/config.toml
-sed -i "s/10.22.33.3/$(nslookup validator3 | grep Address | head -n 2 | tail -n 1 | awk -F ': ' '{print $2}')/g" /home/ec2-user/story/config/config.toml
-sed -i "s/10.22.33.4/$(nslookup validator4 | grep Address | head -n 2 | tail -n 1 | awk -F ': ' '{print $2}')/g" /home/ec2-user/story/config/config.toml
-sed -i "s/10.22.22.1/$(nslookup bootnode1 | grep Address | head -n 2 | tail -n 1 | awk -F ': ' '{print $2}')/g" /home/ec2-user/story/config/config.toml
-sed -i "s/10.22.22.1/$(nslookup bootnode1 | grep Address | head -n 2 | tail -n 1 | awk -F ': ' '{print $2}')/g" /home/ec2-user/geth/config/geth.toml
+cp /binary/* /usr/local/bin
 
-echo "node run..."
-geth --config /home/ec2-user/geth/config/geth.toml --nodekey /home/ec2-user/geth/config/nodekey --metrics --metrics.addr 0.0.0.0 &
-story run --home=/home/ec2-user/story &
+if [ -f "$INIT_DONE_FILE" ]; then
+  echo "skip init, run node..."
+else
+  echo "node $TYPE reset..."
+  rm -rf $DATA_DIR
+  cp -r /config $DATA_DIR
+  geth --state.scheme "hash" --gcmode archive init --datadir="$DATA_DIR/geth/data" $DATA_DIR/geth/config/genesis.json
+  openssl rand -hex 32 > $DATA_DIR/geth/data/geth/jwtsecret
+
+  if [ "$TYPE" = "validator" ]; then
+    sed -i "s/10.22.33.1/$(nslookup $(hostname) | grep Address | head -n 2 | tail -n 1 | awk -F ': ' '{print $2}')/g" $DATA_DIR/story/config/config.toml
+  fi
+
+  sed -i "s/10.22.22.1/$(nslookup bootnode1 | grep Address | head -n 2 | tail -n 1 | awk -F ': ' '{print $2}')/g" $DATA_DIR/story/config/config.toml
+  sed -i "s/10.22.22.1/$(nslookup bootnode1 | grep Address | head -n 2 | tail -n 1 | awk -F ': ' '{print $2}')/g" $DATA_DIR/geth/config/geth.toml
+  touch $INIT_DONE_FILE
+fi
+
+echo "node $TYPE run..."
+geth --config $DATA_DIR/geth/config/geth.toml --nodekey $DATA_DIR/geth/config/nodekey --metrics --metrics.addr 0.0.0.0 &
+story run --home=$DATA_DIR/story &
 
 wait -n
 exit $?
