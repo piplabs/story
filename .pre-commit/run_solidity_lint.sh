a2d45ac620e4dcdabec02b01d9fa8b8be0c1501e
Sebastian Liu <sebsadface@gmail.com>
2024-10-10 17:12:39 -0700

    fix(solhint): rm redundant pre-commit linting hook

--- .pre-commit/run_solidity_lint.sh
+++ .pre-commit/run_solidity_lint.sh
@@ -3,13 +3,11 @@
 # Runs `pnpm lint-check` for every unique foundry project derived from the list
 # of files provided as arguments by pre-commit.
 
-# TODO: Unify and fix solhint versions in repo
-# source scripts/install_foundry.sh
+source scripts/install_foundry.sh
 
-# import foundryroots
-# source .pre-commit/foundry_utils.sh
+source .pre-commit/foundry_utils.sh
 
-# for dir in $(foundryroots $@); do
-#   echo "Running 'lint-check' in ./$dir"
-#   (cd $dir && pnpm lint-check)
-# done
+for dir in $(foundryroots $@); do
+  echo "Running 'lint-fix and lint-full' in ./$dir"
+  (cd $dir && pnpm lint-fix && pnpm lint-full)
+done
