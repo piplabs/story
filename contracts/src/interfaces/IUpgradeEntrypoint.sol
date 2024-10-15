0e5544b23e903bd05995a3f57189dbed0fc24fce
Jongwon Park <contact@parkjongwon.com>
2024-10-15 18:00:46 -0500

    feat: contract upgrade event change

--- contracts/src/interfaces/IUpgradeEntrypoint.sol
+++ contracts/src/interfaces/IUpgradeEntrypoint.sol
@@ -4,24 +4,16 @@ pragma solidity ^0.8.23;
 interface IUpgradeEntrypoint {
     /// PROTO: https://github.com/cosmos/cosmos-sdk/blob/v0.50.9/proto/cosmos/upgrade/v1beta1/upgrade.proto
     /// @notice Emitted when an upgrade is submitted.
-    /// @param name Sets the name for the upgrade. This name will be used by the upgraded version of the software to
-    /// apply any special "on-upgrade" commands during the first BeginBlock method after the upgrade is applied. It is
-    /// also used to detect whether a software version can handle a given upgrade. If no upgrade handler with this name
-    /// has been set in the software, it will be assumed that the software is out-of-date when the upgrade Time or
-    /// Height is reached and the software will exit.
-    /// @param height The height at which the upgrade must be performed.
+    /// @param appVersion The app version of the upgrade.
+    /// @param upgradeHeight The height at which the upgrade must be performed.
     /// @param info Any application specific upgrade info to be included on-chain such as a git commit that validators
     /// could automatically upgrade to.
-    event SoftwareUpgrade(string name, int64 height, string info);
+    event SoftwareUpgrade(string appVersion, int64 upgradeHeight, string info);
 
     /// @notice Submits an upgrade plan.
-    /// @param name Sets the name for the upgrade. This name will be used by the upgraded version of the software to
-    /// apply any special "on-upgrade" commands during the first BeginBlock method after the upgrade is applied. It is
-    /// also used to detect whether a software version can handle a given upgrade. If no upgrade handler with this name
-    /// has been set in the software, it will be assumed that the software is out-of-date when the upgrade Time or
-    /// Height is reached and the software will exit.
-    /// @param height The height at which the upgrade must be performed.
+    /// @param appVersion Sets the app version for the upgrade.
+    /// @param upgradeHeight The height at which the upgrade must be performed.
     /// @param info Any application specific upgrade info to be included on-chain such as a git commit that validators
     /// could automatically upgrade to.
-    function planUpgrade(string calldata name, int64 height, string calldata info) external;
+    function planUpgrade(string calldata appVersion, int64 upgradeHeight, string calldata info) external;
 }
--- contracts/src/protocol/UpgradeEntrypoint.sol
+++ contracts/src/protocol/UpgradeEntrypoint.sol
@@ -21,15 +21,11 @@ contract UpgradeEntrypoint is IUpgradeEntrypoint, Ownable2StepUpgradeable {
     }
 
     /// @notice Submits an upgrade plan.
-    /// @param name Sets the name for the upgrade. This name will be used by the upgraded version of the software to
-    /// apply any special "on-upgrade" commands during the first BeginBlock method after the upgrade is applied. It is
-    /// also used to detect whether a software version can handle a given upgrade. If no upgrade handler with this name
-    /// has been set in the software, it will be assumed that the software is out-of-date when the upgrade Time or
-    /// Height is reached and the software will exit.
-    /// @param height The height at which the upgrade must be performed.
+    /// @param appVersion Sets the app version for the upgrade.
+    /// @param upgradeHeight The height at which the upgrade must be performed.
     /// @param info Any application specific upgrade info to be included on-chain such as a git commit that validators
     /// could automatically upgrade to.
-    function planUpgrade(string calldata name, int64 height, string calldata info) external onlyOwner {
-        emit SoftwareUpgrade({ name: name, height: height, info: info });
+    function planUpgrade(string calldata appVersion, int64 upgradeHeight, string calldata info) external onlyOwner {
+        emit SoftwareUpgrade({ appVersion: appVersion, upgradeHeight: upgradeHeight, info: info });
     }
 }
