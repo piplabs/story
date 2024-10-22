fd6fdc32292dfdf122839cef419500d1c8efb836
0xHansLee <hansol.lee@storyprotocol.xyz>
2024-10-21 22:12:12 -0700

    chore(genesis): update local genesis alloc and exec hash

--- contracts/script/GenerateAlloc.s.sol
+++ contracts/script/GenerateAlloc.s.sol
@@ -381,6 +381,7 @@ contract GenerateAlloc is Script {
             vm.deal(0x00FCeC044cD73e8eC6Ad771556859b00C9011111, 100000000 ether);
             vm.deal(0xb5350B7CaE94C2bF6B2b56Ef6A06cC1153900000, 100000000 ether);
             vm.deal(0x13919a0d8603c35DAC923f92D7E4e1D55e993898, 100000000 ether);
+            vm.deal(0x64a2fdc6f7CD8AA42e0bb59bf80bC47bFFbe4a73, 100000000 ether);
         }
         if (ALLOCATE_10K_TEST_ACCOUNTS && block.chainid != MAINNET_CHAIN_ID) {
             setTestAllocations();
