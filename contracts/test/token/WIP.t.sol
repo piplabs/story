a90e40ee0308b9f055c5e3ff4d61d7fd896d1c54
Kingster <kingter@storyprotocol.xyz>
2024-10-22 20:06:15 -0700

    Introduce Wrapped IP and add to predeploy

---
+++ contracts/test/token/WIP.t.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import { Test } from "../utils/Test.sol";
+import { WIP } from "../../src/token/WIP.sol";
+
+contract ContractWithoutReceive {}
+
+contract WIPTest is Test {
+    function testMetadata() public view {
+        assertEq(wip.name(), "Wrapped IP");
+        assertEq(wip.symbol(), "WIP");
+        assertEq(wip.decimals(), 18);
+    }
+
+    function testFallbackDeposit() public {
+        assertEq(wip.balanceOf(address(this)), 0);
+        assertEq(wip.totalSupply(), 0);
+
+        (bool success, ) = address(wip).call{value: 1 ether}("");
+        assertTrue(success);
+
+        assertEq(wip.balanceOf(address(this)), 1 ether);
+        assertEq(wip.totalSupply(), 1 ether);
+    }
+
+    function testDeposit() public {
+        assertEq(wip.balanceOf(address(this)), 0);
+        assertEq(wip.totalSupply(), 0);
+
+        wip.deposit{value: 1 ether}();
+
+        assertEq(wip.balanceOf(address(this)), 1 ether);
+        assertEq(wip.totalSupply(), 1 ether);
+    }
+
+    function testWithdraw() public {
+        uint256 startingBalance = address(this).balance;
+
+        wip.deposit{value: 1 ether}();
+
+        wip.withdraw(1 ether);
+
+        uint256 balanceAfterWithdraw = address(this).balance;
+
+        assertEq(balanceAfterWithdraw, startingBalance);
+        assertEq(wip.balanceOf(address(this)), 0);
+        assertEq(wip.totalSupply(), 0);
+    }
+
+    function testPartialWithdraw() public {
+        wip.deposit{value: 1 ether}();
+
+        uint256 balanceBeforeWithdraw = address(this).balance;
+
+        wip.withdraw(0.5 ether);
+
+        uint256 balanceAfterWithdraw = address(this).balance;
+
+        assertEq(balanceAfterWithdraw, balanceBeforeWithdraw + 0.5 ether);
+        assertEq(wip.balanceOf(address(this)), 0.5 ether);
+        assertEq(wip.totalSupply(), 0.5 ether);
+    }
+
+    function testWithdrawToContractWithoutReceiveReverts() public {
+        address owner = address(new ContractWithoutReceive());
+
+        vm.deal(owner, 1 ether);
+
+        vm.prank(owner);
+        wip.deposit{value: 1 ether}();
+
+        assertEq(wip.balanceOf(owner), 1 ether);
+
+        vm.expectRevert(WIP.IPTransferFailed.selector);
+        vm.prank(owner);
+        wip.withdraw(1 ether);
+    }
+
+    function testFallbackDeposit(uint256 amount) public {
+        amount = _bound(amount, 0, address(this).balance);
+
+        assertEq(wip.balanceOf(address(this)), 0);
+        assertEq(wip.totalSupply(), 0);
+
+        (bool success, ) = address(wip).call{value: amount}("");
+        assertTrue(success);
+
+        assertEq(wip.balanceOf(address(this)), amount);
+        assertEq(wip.totalSupply(), amount);
+    }
+
+    function testDeposit(uint256 amount) public {
+        amount = _bound(amount, 0, address(this).balance);
+
+        assertEq(wip.balanceOf(address(this)), 0);
+        assertEq(wip.totalSupply(), 0);
+
+        wip.deposit{value: amount}();
+
+        assertEq(wip.balanceOf(address(this)), amount);
+        assertEq(wip.totalSupply(), amount);
+    }
+
+    function testWithdraw(uint256 depositAmount, uint256 withdrawAmount) public {
+        depositAmount = _bound(depositAmount, 0, address(this).balance);
+        withdrawAmount = _bound(withdrawAmount, 0, depositAmount);
+
+        wip.deposit{value: depositAmount}();
+
+        uint256 balanceBeforeWithdraw = address(this).balance;
+
+        wip.withdraw(withdrawAmount);
+
+        uint256 balanceAfterWithdraw = address(this).balance;
+
+        assertEq(balanceAfterWithdraw, balanceBeforeWithdraw + withdrawAmount);
+        assertEq(wip.balanceOf(address(this)), depositAmount - withdrawAmount);
+        assertEq(wip.totalSupply(), depositAmount - withdrawAmount);
+    }
+
+    receive() external payable {}
+}
+
