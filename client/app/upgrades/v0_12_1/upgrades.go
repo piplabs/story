243222457fbad6fe8401b02c95c691808a9af7a1
limengformal <meng.li@storyprotocol.xyz>
2024-10-28 13:04:27 -0700

    update staking periods for testnet

--- client/app/upgrades/v0_12_1/upgrades.go
+++ client/app/upgrades/v0_12_1/upgrades.go
@@ -3,6 +3,7 @@ package v0_12_1
 
 import (
 	"context"
+	"time"
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 
@@ -24,6 +25,48 @@ func CreateUpgradeHandler(
 		vm, err := mm.RunMigrations(ctx, configurator, vm)
 		if err != nil {
 			return vm, errors.Wrap(err, "run migrations")
+		}
+
+		clog.Info(ctx, "Decreasing staking periods...")
+		stakingParams, err := keepers.StakingKeeper.GetParams(ctx)
+		if err != nil {
+			return vm, errors.Wrap(err, "failed to get staking params")
+		}
+		periods := stakingParams.GetPeriods()
+
+		for _, p := range periods {
+			if p.PeriodType == 1 {
+				clog.Info(ctx, "Existing short period duration: %d", p.Duration)
+				p.Duration = time.Hour * 7 * 24 // one week in hours
+			} else if p.PeriodType == 2 {
+				clog.Info(ctx, "Existing medium period duration: %d", p.Duration)
+				p.Duration = time.Hour * 14 * 24 // two week in hours
+			} else if p.PeriodType == 3 {
+				clog.Info(ctx, "Existing long period duration: %d", p.Duration)
+				p.Duration = time.Hour * 28 * 24 // four week in hours
+			}
+		}
+
+		err = keepers.StakingKeeper.SetParams(ctx, stakingParams)
+		if err != nil {
+			return vm, errors.Wrap(err, "failed to set staking params")
+		}
+
+		clog.Info(ctx, "Checking newstaking periods...")
+		stakingParams, err = keepers.StakingKeeper.GetParams(ctx)
+		if err != nil {
+			return vm, errors.Wrap(err, "failed to get staking params")
+		}
+		periods = stakingParams.GetPeriods()
+
+		for _, p := range periods {
+			if p.PeriodType == 1 {
+				clog.Info(ctx, "New short period duration: %d", p.Duration)
+			} else if p.PeriodType == 2 {
+				clog.Info(ctx, "New medium period duration: %d", p.Duration)
+			} else if p.PeriodType == 3 {
+				clog.Info(ctx, "New long period duration: %d", p.Duration)
+			}
 		}
 
 		clog.Info(ctx, "Upgrade v0.12.1 complete")
