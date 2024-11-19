e997bf1e663082ca7d149cba92579b18f0199f25
ezreal1997 <gezerui1997@gmail.com>
2024-11-19 15:47:39 -0800

    fix(evmstaking): panic handling

--- client/x/evmengine/keeper/db.go
+++ client/x/evmengine/keeper/db.go
@@ -38,7 +38,7 @@ func (k *Keeper) InsertGenesisHead(ctx context.Context, executionBlockHash []byt
 func (k *Keeper) getExecutionHead(ctx context.Context) (*ExecutionHead, error) {
 	head, err := k.headTable.Get(ctx, executionHeadID)
 	if err != nil {
-		return nil, errors.Wrap(err, "update execution head")
+		return nil, errors.Wrap(err, "get execution head")
 	}
 
 	return head, nil
