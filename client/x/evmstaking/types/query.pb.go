b9fd6034b07a762e76316e6fa27a4e56278721cd
Jongwon Park <contact@parkjongwon.com>
2024-12-13 00:32:04 +0900

    nit(*): buf fix

--- client/x/evmstaking/types/query.pb.go
+++ client/x/evmstaking/types/query.pb.go
@@ -358,6 +358,7 @@ func _Query_GetWithdrawalQueue_Handler(srv interface{}, ctx context.Context, dec
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "story.evmstaking.v1.types.Query",
 	HandlerType: (*QueryServer)(nil),
