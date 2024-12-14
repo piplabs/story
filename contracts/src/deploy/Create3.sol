17eaf993cfc6dea113f2f25639115a5e3eed50ae
Kingter <83567446+kingster-will@users.noreply.github.com>
2024-12-13 17:12:07 -0800

    feat(create3): allow deployed solely based on salt (#437)
    Enhance pre-deployed CREATE3 to support deploying contracts to
    deterministic addresses calculated solely based on the salt, independent
    of the deployer address.
    
    issue: none

--- contracts/src/deploy/Create3.sol
+++ contracts/src/deploy/Create3.sol
@@ -6,9 +6,11 @@ import { CREATE3 } from "solmate/src/utils/CREATE3.sol";
 /**
  * @title Create3
  * @notice Factory for deploying contracts to deterministic addresses via CREATE3 Enables deploying
- *         contracts using CREATE3. Each deployer (msg.sender) has its own namespace for deployed
- *         addresses.
- * @author zefram.eth
+ *         contracts using CREATE3.
+ *         It is based on Zefram’s implementation and includes support for deployment using only a salt value.
+ *         It support two deployment styles:
+ *         1. Each deployer (msg.sender) has its own namespace for deployed addresses.
+ *         2. Deterministic deployment using a single salt.
  * @custom:attribution zefram.eth (https://github.com/ZeframLou/create3-factory/blob/main/src/CREATE3Factory.sol)
  */
 contract Create3 {
@@ -35,6 +37,26 @@ contract Create3 {
     function getDeployed(address deployer, bytes32 salt) external view returns (address deployed) {
         // hash salt with the deployer address to give each deployer its own namespace
         salt = keccak256(abi.encodePacked(deployer, salt));
+        return CREATE3.getDeployed(salt);
+    }
+
+    /**
+     * @notice Deploys a contract using CREATE3
+     * @param salt          The deployer-specific salt for determining the deployed contract's address
+     * @param creationCode  The creation code of the contract to deploy
+     * @return deployed     The address of the deployed contract
+     */
+    function deployDeterministic(bytes memory creationCode, bytes32 salt) external payable returns (address deployed) {
+        return CREATE3.deploy(salt, creationCode, msg.value);
+    }
+
+    /**
+     * @notice Predicts the address of a deployed contract
+     * @param salt      The deployer-specific salt for determining the deployed contract's address
+     * @return deployed The address of the contract that will be deployed
+     */
+    function predictDeterministicAddress(bytes32 salt) external view returns (address deployed) {
+        // hash salt with the deployer address to give each deployer its own namespace
         return CREATE3.getDeployed(salt);
     }
 }
