2dd5638879c3021c42aa7cb418a9d3bd1702b9b8
Meng <69021250+limengformal@users.noreply.github.com>
2025-01-18 19:15:04 -0800

    chore(release): finalize story 1.0.0 stable release (#86)

--- lib/buildinfo/buildinfo.go
+++ lib/buildinfo/buildinfo.go
@@ -13,9 +13,9 @@ import (
 )
 
 const (
-	VersionMajor = 0        // Major version component of the current release
-	VersionMinor = 15       // Minor version component of the current release
-	VersionPatch = 1        // Patch version component of the current release
+	VersionMajor = 1        // Major version component of the current release
+	VersionMinor = 0        // Minor version component of the current release
+	VersionPatch = 0        // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
