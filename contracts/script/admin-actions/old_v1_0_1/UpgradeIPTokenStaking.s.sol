1c0b6e340b660528e3fbd6fc21684bf6ea798fb6
Raul <raul.mf86@gmail.com>
2025-05-10 02:39:21 -0300

    refactor for batch timelock and migration scripts

---
+++ contracts/script/admin-actions/old_v1_0_1/UpgradeIPTokenStaking.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { UpgradeTransparentProxy } from "../../utils/UpgradeTransparentProxy.s.sol";
+
+/// @notice Script to upgrade the IPTokenStaking contract through a timelock
+contract UpgradeIpTokenStaking is UpgradeTransparentProxy {
+    constructor()
+        UpgradeTransparentProxy(
+            "upgrade-staking-v1_0_1", // file name
+            address(0), // timelock address (current timelock)
+            vm.envAddress("PROPOSER_ADDRESS"), // Schedule proposer address
+            bytes32(0) // salt
+        )
+    {}
+
+    function _getAddresses()
+        internal
+        view
+        override
+        returns (
+            address[] memory proxyAddresses,
+            address[] memory newImplementationAddresses,
+            address[] memory proxyAdminAddresses
+        )
+    {
+        proxyAddresses = new address[](1);
+        proxyAddresses[0] = vm.envAddress("PROXY_ADDRESS");
+        newImplementationAddresses = new address[](1);
+        newImplementationAddresses[0] = vm.envAddress("NEW_IMPLEMENTATION_ADDRESS");
+        proxyAdminAddresses = new address[](1);
+        proxyAdminAddresses[0] = vm.envAddress("PROXY_ADMIN_ADDRESS");
+        return (proxyAddresses, newImplementationAddresses, proxyAdminAddresses);
+    }
+}
