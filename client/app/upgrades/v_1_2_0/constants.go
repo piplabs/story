6d6af4ee788f37b7fdc2715308d9545124f3f87f
Yao <lucas952222@gmail.com>
2025-05-14 15:43:10 +0800

    update height

--- client/app/upgrades/v_1_2_0/constants.go
+++ client/app/upgrades/v_1_2_0/constants.go
@@ -19,7 +19,7 @@ const (
 	StoryUpgradeHeight = 4477880
 
 	// DevnetUpgradeHeight defines the block height at which virgil upgrade is triggered on Internal Devnet.
-	DevnetUpgradeHeight = 4477880
+	DevnetUpgradeHeight = 10
 
 	// new max bytes for consensus params.
 	newMaxBytes = 20971520 // 20MB
