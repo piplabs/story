906510ccb3e07bfd91646a35c06d5e0f42de5372
jia57b <jia@57blocks.com>
2025-06-06 20:19:44 +0800

    update height

--- client/app/upgrades/v_1_3_0/constants.go
+++ client/app/upgrades/v_1_3_0/constants.go
@@ -19,7 +19,7 @@ const (
 	StoryUpgradeHeight = 10000000
 
 	// DevnetUpgradeHeight defines the block height at which virgil upgrade is triggered on Internal Devnet.
-	DevnetUpgradeHeight = 13000
+	DevnetUpgradeHeight = 8000
 
 	NewMaxValidators = 80
 )
