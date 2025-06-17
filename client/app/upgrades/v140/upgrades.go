8afd43c6d73d3032e96d3c0d3a471f0a13b64928
0xHansLee <hansol.lee@storyprotocol.xyz>
2025-06-18 00:25:33 +0900

    feat(evmengine): defer upgrade scheduling to upgrade keeper

---
+++ client/app/upgrades/v140/upgrades.go
@@ -0,0 +1,24 @@
+package v140
+
+import (
+	"context"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/piplabs/story/client/app/keepers"
+	"github.com/piplabs/story/lib/log"
+)
+
+func CreateUpgradeHandler(
+	_ *module.Manager,
+	_ module.Configurator,
+	_ *keepers.Keepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		log.Info(ctx, "The upgrade to v1.4.0")
+
+		return vm, nil
+	}
+}
