88092177717ca9c735eb8177e95d2610e639dcfd
0xHansLee <hansol.lee@storyprotocol.xyz>
2025-09-12 14:57:20 +0900

    chore(evmengine): preserve error code 1

--- client/x/evmengine/types/errors.go
+++ client/x/evmengine/types/errors.go
@@ -3,6 +3,6 @@ package types
 import "cosmossdk.io/errors"
 
 var (
-	ErrUpgradePending  = errors.Register(ModuleName, 1, "upgrade is already pending")
-	ErrUpgradeNotFound = errors.Register(ModuleName, 2, "upgrade not found")
+	ErrUpgradePending  = errors.Register(ModuleName, 2, "upgrade is already pending")
+	ErrUpgradeNotFound = errors.Register(ModuleName, 3, "upgrade not found")
 )
