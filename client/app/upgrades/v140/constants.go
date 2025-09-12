ed4d0d406d67249736159f4c658f612becc89424
0xHansLee <hansol.lee@storyprotocol.xyz>
2025-09-12 14:43:33 +0900

    fix(app): consistent usage of upgrade height for v1.4

--- client/app/upgrades/v140/constants.go
+++ client/app/upgrades/v140/constants.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/piplabs/story/client/app/keepers"
 	"github.com/piplabs/story/client/app/upgrades"
+	"github.com/piplabs/story/lib/log"
 	"github.com/piplabs/story/lib/netconf"
 )
 
@@ -20,5 +21,15 @@ var Fork = upgrades.Fork{
 	UpgradeName:    netconf.V140,
 	UpgradeInfo:    "v140 upgrade",
 	BeginForkLogic: func(_ sdk.Context, _ *keepers.Keepers) {},
-	UpgradeHeight:  100000000, // TODO: set fork height depend on the network
+}
+
+func GetUpgradeHeight(ctx sdk.Context) (int64, bool) {
+	height, err := netconf.GetUpgradeHeight(ctx.ChainID(), netconf.V140)
+	if err != nil {
+		log.Error(ctx, "Failed to get upgrade height", err, "chain_id", ctx.ChainID(), "upgrade_name", netconf.V140)
+
+		return 0, false
+	}
+
+	return height, true
 }
