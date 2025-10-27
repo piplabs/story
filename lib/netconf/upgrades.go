83e3fc1d088f08aaa493d0e9365b9e64fcb856b0
Yao <lucas952222@gmail.com>
2025-10-27 15:06:13 +0800

    test: Terence upgrade

--- lib/netconf/upgrades.go
+++ lib/netconf/upgrades.go
@@ -25,7 +25,7 @@ type UpgradeMap map[string]int64
 var UpgradeHistories = map[string]UpgradeMap{
 	TestChainID: {
 		V121:    10,
-		Terence: 50,
+		Terence: 50, // internal-devnet test
 	},
 	LocalChainID: {
 		V121:    0,
