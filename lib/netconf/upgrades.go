c16fd9d352d0c33be111380888272a23893f7be6
Yao <lucas952222@gmail.com>
2025-10-31 01:17:36 +0800

    chore

--- lib/netconf/upgrades.go
+++ lib/netconf/upgrades.go
@@ -24,8 +24,8 @@ type UpgradeMap map[string]int64
 // UpgradeHistories are the map of histories for each network.
 var UpgradeHistories = map[string]UpgradeMap{
 	TestChainID: {
-		V121:    0,
-		Terence: 3400, // internal-devnet test
+		V121:    10,
+		Terence: 3500, // internal-devnet test
 	},
 	LocalChainID: {
 		V121:    0,
