ce1607713b6550efbe58a7440519bc7ade68549c
steven <wangpeculiar@gmail.com>
2026-03-24 21:21:11 +0800

    skip threshold request only when there's no active round yet, resharing should not reject requests

--- client/x/dkg/keeper/dkg_handler.go
+++ client/x/dkg/keeper/dkg_handler.go
@@ -434,12 +434,15 @@ func (k *Keeper) ThresholdDecryptRequested(ctx context.Context, round uint32, re
 		return errors.Wrap(err, "failed to get dkg network for decrypt request")
 	}
 
-	if dkgNetwork.Stage != types.DKGStageActive {
-		log.Info(ctx, "Skipping threshold decrypt request; DKG round is not active",
+	latestRound, err := k.GetLatestActiveRound(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get latest active DKG round")
+	}
+	if latestRound == nil {
+		log.Info(ctx, "Skipping threshold decrypt request; No active DKG round yet",
 			"round", round,
 			"stage", dkgNetwork.Stage.String(),
 		)
-
 		return nil
 	}
 
